071fa43f81bbda10761f4cb795843268e3a39af4
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-05-13 15:23:29 +0600

    adding the image

--- player.py
+++ player.py
@@ -1,17 +1,20 @@
 from settings import *
 from pygame.sprite import Group
 from timer import Timer
-
+from os.path import join
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_sprites, semi_collision_sprites):
         super().__init__(groups)
-        self.image = pygame.Surface((48,56))
-        self.image.fill("red")
+        # self.image = pygame.Surface((48,56))
+        self.image = pygame.image.load(join("..", "graphics", "player", "idle", "0.png"))
+        # self.image = pygame.image.load(join("..", "graphics", "player", "idle", "idle_1.png"))
+        # self.image.fill("red")
         
         #rects
         self.rect = self.image.get_frect(topleft = pos)
+        self.hitbox_rect = self.rect.inflate(-76, -36)
         #this is to check from which direction the rect is coming from to check for the collision. so it stores the previous position 
-        self.old_rect = self.rect.copy()
+        self.old_rect = self.hitbox_rect.copy()
         
         #player movements variables
         self.direction = vector()
@@ -56,7 +59,7 @@ class Player(pygame.sprite.Sprite):
     #player movements based on inputs
     def move(self, dt):
         #horizontal movements
-        self.rect.x += self.direction.x * self.speed * dt
+        self.hitbox_rect.x += self.direction.x * self.speed * dt
         #check for collision in x axis
         #wall slide
         self.collision("horizontal")
@@ -64,22 +67,23 @@ class Player(pygame.sprite.Sprite):
             self.direction.y = 0
 
             # change the gravity style when hit the walls
-            self.rect.y += self.gravity / 10 * dt
+            self.hitbox_rect.y += self.gravity / 10 * dt
         else:
             #virtical movements
             self.direction.y += self.gravity / 2 * dt #for the accelaration of gravty
-            self.rect.y += self.direction.y * dt
+            self.hitbox_rect.y += self.direction.y * dt
             self.direction.y += self.gravity / 2 * dt #for the accelaration of gravty
         #check for virtical movements
         self.collision("virtical")
         self.semi_collision()
+        self.rect.center = self.hitbox_rect.center
         
         
         if self.jump:
             if self.on_surface["floor"]:
                 self.direction.y = self.jump_height
                 self.timer["wall slide block"].activate()
-                self.rect.bottom -= 1
+                self.hitbox_rect.bottom -= 1
             elif any((self.on_surface["left"], self.on_surface["right"])) and not self.timer["wall slide block"].active:
                 self.timer["wall jump"].activate()
                 self.direction.y = self.jump_height
@@ -91,27 +95,27 @@ class Player(pygame.sprite.Sprite):
         
     def collision(self, axis):
         for sprite in self.collision_sprites:
-            if sprite.rect.colliderect(self.rect):
+            if sprite.rect.colliderect(self.hitbox_rect):
                 #horizontal collisions
                 if axis == "horizontal":
                     #left side collision check
-                    if self.rect.left <= sprite.rect.right and int(self.old_rect.left) >= sprite.old_rect.right:
-                        self.rect.left = sprite.rect.right
+                    if self.hitbox_rect.left <= sprite.rect.right and int(self.old_rect.left) >= sprite.old_rect.right:
+                        self.hitbox_rect.left = sprite.rect.right
                     #right side collision
-                    if self.rect.right >= sprite.rect.left and int(self.old_rect.right) <= sprite.old_rect.left:
-                        self.rect.right = sprite.rect.left
+                    if self.hitbox_rect.right >= sprite.rect.left and int(self.old_rect.right) <= sprite.old_rect.left:
+                        self.hitbox_rect.right = sprite.rect.left
                         
                 
                 if axis == "virtical":
                     #top collision
-                    if self.rect.top <= sprite.rect.bottom and int(self.old_rect.top) >= sprite.old_rect.bottom:
-                        self.rect.top = sprite.rect.bottom
+                    if self.hitbox_rect.top <= sprite.rect.bottom and int(self.old_rect.top) >= sprite.old_rect.bottom:
+                        self.hitbox_rect.top = sprite.rect.bottom
                         if hasattr(sprite, "moving"):
-                            self.rect.top += 6
+                            self.hitbox_rect.top += 6
                         
                     #bottom collision
-                    if self.rect.bottom >= sprite.rect.top and int(self.old_rect.bottom) <= sprite.old_rect.top:
-                        self.rect.bottom = sprite.rect.top
+                    if self.hitbox_rect.bottom >= sprite.rect.top and int(self.old_rect.bottom) <= sprite.old_rect.top:
+                        self.hitbox_rect.bottom = sprite.rect.top
                     
                     #if collision is found virtically then gravity shouldn't work
                     self.direction.y = 0
@@ -120,22 +124,22 @@ class Player(pygame.sprite.Sprite):
         if not self.timer["platform skip"].active:
             for sprite in self.semi_collision_sprites:
                 if sprite.rect.colliderect(self.rect):
-                    if self.rect.bottom >= sprite.rect.top and int(self.old_rect.bottom) <= sprite.old_rect.top:
-                        self.rect.bottom = sprite.rect.top
+                    if self.hitbox_rect.bottom >= sprite.rect.top and int(self.old_rect.bottom) <= sprite.old_rect.top:
+                        self.hitbox_rect.bottom = sprite.rect.top
                         if self.direction.y > 0:
                             self.direction.y = 0
                 
     #to make sure that player is moving with the moving platform
     def platform_move(self, dt):
         if self.platform:
-            self.rect.topleft += self.platform.direction * self.platform.speed * dt
+            self.hitbox_rect.topleft += self.platform.direction * self.platform.speed * dt
     
     def check_contact(self):
-        floor_rect = pygame.Rect(self.rect.bottomleft, (self.rect.width, 2))
+        floor_rect = pygame.Rect(self.hitbox_rect.bottomleft, (self.hitbox_rect.width, 2))
         
-        right_side_rect = pygame.Rect(self.rect.topright + vector(0, self.rect.height / 4), (2, self.rect.height / 2))
+        right_side_rect = pygame.Rect(self.hitbox_rect.topright + vector(0, self.hitbox_rect.height / 4), (2, self.hitbox_rect.height / 2))
         
-        left_side_rect = pygame.Rect(self.rect.topleft + vector(-2, self.rect.height / 4), (2, self.rect.height / 2))
+        left_side_rect = pygame.Rect(self.hitbox_rect.topleft + vector(-2, self.hitbox_rect.height / 4), (2, self.hitbox_rect.height / 2))
         
         
         collision_rects = [sprite.rect for sprite in self.collision_sprites]
@@ -161,7 +165,7 @@ class Player(pygame.sprite.Sprite):
     
     #run all methods in this method
     def update(self, dt):
-        self.old_rect = self.rect.copy()
+        self.old_rect = self.hitbox_rect.copy()
         self.update_timer()
         self.input()
         self.move(dt)
