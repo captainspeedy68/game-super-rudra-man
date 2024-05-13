e245c7a0d1480c64a17bcd653403f5356b6ed99b
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-05-13 12:57:59 +0600

    creating a moving sprites class

--- sprites.py
+++ sprites.py
@@ -4,11 +4,17 @@ from settings import *
 
 #createing a sprite class and that inherits a Sprite class from pygame
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, pos, surf, groups):
+    def __init__(self, pos, surf = pygame.Surface((TILE_SIZE,TILE_SIZE)), groups = None):
         super().__init__(groups)
-        self.image = pygame.Surface((TILE_SIZE,TILE_SIZE))
+        self.image = surf
         self.image.fill("white")
         
         #rect
         self.rect = self.image.get_frect(topleft = pos)
         self.old_rect = self.rect.copy()
+        
+class MovingSprites(Sprite):
+    def __init__(self, groups, start_pos, end_pos, move_dir, speed):
+        surf = pygame.Surface((200, 50))
+        super().__init__(start_pos, surf, groups)
+        self.rect.center = start_pos
