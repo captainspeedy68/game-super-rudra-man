e2afaa8a1b6c08167f4eb353d64014fc8de5769b
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-05-13 12:57:26 +0600

    initial addition of moving levels

--- level.py
+++ level.py
@@ -1,5 +1,5 @@
 from settings import *
-from sprites import Sprite
+from sprites import Sprite, MovingSprites
 from player import Player
 class Level:
     def __init__(self, tmx_map):
@@ -17,6 +17,7 @@ class Level:
         
         
     def setup(self, tmx_map):
+        #tiles
         
         #call the component layer by name from maps and distructure properties to use in for loop
         for x, y, surf in tmx_map.get_layer_by_name("Terrain").tiles():
@@ -24,11 +25,26 @@ class Level:
             #creating instance of sprite class and pixelising the position
             Sprite((x * TILE_SIZE,y * TILE_SIZE), surf, (self.all_sprites, self.collsion_sprites))
         
+        #objects
         #getting the player object
         for obj in tmx_map.get_layer_by_name("Objects"):
             if obj.name == "player":
                 Player((obj.x, obj.y), self.all_sprites, self.collsion_sprites)
                 
+                
+        #moving objects
+        for obj in tmx_map.get_layer_by_name("Moving Objects"):
+            if obj.name == "helicopter":
+                if (obj.width > obj.height): #horizontal movement
+                    move_dir = "x"
+                    start_pos = (obj.x, obj.y + obj.height / 2)
+                    end_pos = (obj.x + obj.width, obj.y + obj.height / 2)
+                else: #virtical movement
+                    move_dir = "y"
+                    start_pos = (obj.x + obj.width / 2, obj.y)
+                    end_pos = (obj.x + obj.width / 2, obj.y + obj.height)
+                speed = obj.properties["speed"]
+                MovingSprites(self.all_sprites, start_pos, end_pos, move_dir, speed)
                 
     def run(self, dt):
         self.all_sprites.update(dt)
