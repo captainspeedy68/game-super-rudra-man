d0c326376aec0c848b1fcf06c57a1313179086f8
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-05-18 12:32:46 +0600

    health setter getter

--- data.py
+++ data.py
@@ -19,3 +19,12 @@ class Data:
             self.coins -= 100
             self.health += 1
         self.ui.show_coins(self.coins)
+        
+    @property
+	def health(self):
+		return self._health
+
+	@health.setter
+	def health(self, value):
+		self._health = value
+		self.ui.create_hearts(value)
