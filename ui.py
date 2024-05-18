ad1d9a3b04a8da3df57cd983941e63e6251557db
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-05-18 12:22:53 +0600

    hearts animations

---
+++ ui.py
@@ -0,0 +1,24 @@
+from settings import * 
+from sprites import AnimatedSprite
+from random import randint
+from timer import Timer
+
+class Heart(AnimatedSprite):
+	def __init__(self, pos, frames, groups):
+		super().__init__(pos, frames, groups)
+		self.active = False
+
+	def animate(self, dt):
+		self.frame_index += ANIMATION_SPEED * dt
+		if self.frame_index < len(self.frames):
+			self.image = self.frames[int(self.frame_index)]
+		else:
+			self.active = False
+			self.frame_index = 0
+
+	def update(self, dt):
+		if self.active:
+			self.animate(dt)
+		else:
+			if randint(0,2000) == 1:
+				self.active = True
