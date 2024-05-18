b91d8fb18996c8aac5dfd5d8c8b64b434eb9c9ba
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-05-18 12:31:29 +0600

    coin getter setter

--- data.py
+++ data.py
@@ -1,9 +1,21 @@
 class Data:
-	def __init__(self, ui):
-		self.ui = ui
-		self._coins = 0
-		self._health = 5
-		self.ui.create_hearts(self._health)
+    def __init__(self, ui):
+        self.ui = ui
+        self._coins = 0
+        self._health = 5
+        self.ui.create_hearts(self._health)
 
-		self.unlocked_level = 0
-		self.current_level = 0
+        self.unlocked_level = 0
+        self.current_level = 0
+
+    @property
+    def coins(self):
+        return self._coins
+
+    @coins.setter
+    def coins(self, value):
+        self._coins = value
+        if self.coins >= 100:
+            self.coins -= 100
+            self.health += 1
+        self.ui.show_coins(self.coins)
