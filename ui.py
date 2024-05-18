319a1908666da9f6a6ef99926087e8c89d1a7054
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-05-18 12:24:01 +0600

    making a basic ui class

--- ui.py
+++ ui.py
@@ -3,6 +3,22 @@ from sprites import AnimatedSprite
 from random import randint
 from timer import Timer
 
+class UI:
+	def __init__(self, font, frames):
+		self.display_surface = pygame.display.get_surface()
+		self.sprites = pygame.sprite.Group()
+		self.font = font
+
+		# health / hearts 
+		self.heart_frames = frames['heart']
+		self.heart_surf_width = self.heart_frames[0].get_width()
+		self.heart_padding = 6
+
+		# coins 
+		self.coin_amount = 0
+		self.coin_timer = Timer(1000)
+		self.coin_surf = frames['coin']
+
 class Heart(AnimatedSprite):
 	def __init__(self, pos, frames, groups):
 		super().__init__(pos, frames, groups)
