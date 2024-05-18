c3fd6077b53f2e475ab5c617a8dd712f4419b421
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-05-18 21:35:53 +0600

    enemy pearls

--- enemies.py
+++ enemies.py
@@ -41,3 +41,29 @@ class Tooth(pygame.sprite.Sprite):
 		   floor_rect_left.collidelist(self.collision_rects) < 0 and self.direction < 0 or \
 		   wall_rect.collidelist(self.collision_rects) != -1:
 			self.direction *= -1
+
+
+class Pearl(pygame.sprite.Sprite):
+	def __init__(self, pos, groups, surf, direction, speed):
+		self.pearl = True
+		super().__init__(groups)
+		self.image = surf
+		self.rect = self.image.get_frect(center = pos + vector(50 * direction,0))
+		self.direction = direction
+		self.speed = speed
+		self.z = Z_LAYERS['main']
+		self.timers = {'lifetime': Timer(5000), 'reverse': Timer(250)}
+		self.timers['lifetime'].activate()
+
+	def reverse(self):
+		if not self.timers['reverse'].active:
+			self.direction *= -1 
+			self.timers['reverse'].activate()
+
+	def update(self, dt):
+		for timer in self.timers.values():
+			timer.update()
+
+		self.rect.x += self.direction * self.speed * dt
+		if not self.timers['lifetime'].active:
+			self.kill()
