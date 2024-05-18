c27fc7bbfde1638529c657cfc8c65381e67ed1b0
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-05-19 00:23:38 +0600

    adding fonts

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from settings import * 
+from settings import *
 from level import Level
 from pytmx.util_pygame import load_pygame
 from os.path import join
-from support import * 
+from support import *
 from data import Data
 from debug import debug
 from ui import UI
+
+
+# define font
+font_large = pygame.font.Font(join('..', 'fonts', 'turok.ttf'), 80)
+font_small = pygame.font.Font(join('..', 'fonts', 'turok.ttf'), 40)
+
+
+# function for outputting text onto the screen
 
 
 class Game:
@@ -18,7 +26,7 @@ class Game:
 
 		self.ui = UI(self.font, self.ui_frames)
 		self.data = Data(self.ui)
-          #create dictionary to pygame load files... give the path using join so that it works in every os
+          # create dictionary to pygame load files... give the path using join so that it works in every os
 		self.tmx_maps = {
 			# 0: load_pygame(join('..', 'data', 'levels', 'omni.tmx')),
 			0: load_pygame(join('..', 'data', 'levels', '1.tmx')),
@@ -27,17 +35,19 @@ class Game:
 			# 0:  load_pygame(join('..', 'data', 'levels', '4.tmx')),
 			# 0: load_pygame(join('..', 'data', 'levels', '5.tmx')),
 			}
-		self.tmx_overworld = load_pygame(join('..', 'data', 'overworld', 'overworld.tmx'))
-		self.current_stage = Level(self.tmx_maps[self.data.current_level], self.level_frames, self.audio_files, self.data, self.switch_stage)
+		self.tmx_overworld = load_pygame(
+		    join('..', 'data', 'overworld', 'overworld.tmx'))
+		self.current_stage = Level(self.tmx_maps[self.data.current_level],
+		                           self.level_frames, self.audio_files, self.data, self.switch_stage)
 		# self.bg_music.play(-1)
 
-
-	def switch_stage(self, target, unlock = 0):
-      #create levels and send map to the level
+	def switch_stage(self, target, unlock=0):
+      # create levels and send map to the level
 		if target == 'level':
-			self.current_stage = Level(self.tmx_maps[self.data.current_level], self.level_frames, self.audio_files, self.data, self.switch_stage)
-			
-		# else: # overworld 
+			self.current_stage = Level(self.tmx_maps[self.data.current_level],
+			                           self.level_frames, self.audio_files, self.data, self.switch_stage)
+
+		# else: # overworld
 		# 	if unlock > 0:
 		# 		self.data.unlocked_level = 6
 		# 	else:
@@ -48,35 +58,36 @@ class Game:
 		self.level_frames = {
 			'flag': import_folder('..', 'graphics', 'level', 'flag'),
 			'saw': import_folder('..', 'graphics', 'enemies', 'saw', 'animation'),
-			'floor_spike': import_folder('..', 'graphics','enemies', 'floor_spikes'),
+			'floor_spike': import_folder('..', 'graphics', 'enemies', 'floor_spikes'),
 			'palms': import_sub_folders('..', 'graphics', 'level', 'palms'),
-			'candle': import_folder('..', 'graphics','level', 'candle'),
-			'window': import_folder('..', 'graphics','level', 'window'),
-			'big_chain': import_folder('..', 'graphics','level', 'big_chains'),
-			'small_chain': import_folder('..', 'graphics','level', 'small_chains'),
-			'candle_light': import_folder('..', 'graphics','level', 'candle light'),
-			'player': import_sub_folders('..', 'graphics','player'),
+			'candle': import_folder('..', 'graphics', 'level', 'candle'),
+			'window': import_folder('..', 'graphics', 'level', 'window'),
+			'big_chain': import_folder('..', 'graphics', 'level', 'big_chains'),
+			'small_chain': import_folder('..', 'graphics', 'level', 'small_chains'),
+			'candle_light': import_folder('..', 'graphics', 'level', 'candle light'),
+			'player': import_sub_folders('..', 'graphics', 'player'),
 			'saw': import_folder('..', 'graphics', 'enemies', 'saw', 'animation'),
 			'saw_chain': import_image('..',  'graphics', 'enemies', 'saw', 'saw_chain'),
 			'helicopter': import_folder('..', 'graphics', 'level', 'helicopter'),
 			'boat': import_folder('..',  'graphics', 'objects', 'boat'),
 			'spike': import_image('..',  'graphics', 'enemies', 'spike_ball', 'Spiked Ball'),
 			'spike_chain': import_image('..',  'graphics', 'enemies', 'spike_ball', 'spiked_chain'),
-			'tooth': import_folder('..', 'graphics','enemies', 'tooth', 'run'),
-			'shell': import_sub_folders('..', 'graphics','enemies', 'shell'),
+			'tooth': import_folder('..', 'graphics', 'enemies', 'tooth', 'run'),
+			'shell': import_sub_folders('..', 'graphics', 'enemies', 'shell'),
 			'pearl': import_image('..',  'graphics', 'enemies', 'bullets', 'pearl'),
 			'items': import_sub_folders('..', 'graphics', 'items'),
 			'particle': import_folder('..', 'graphics', 'effects', 'particle'),
 			'water_top': import_folder('..', 'graphics', 'level', 'water', 'top'),
 			'water_body': import_image('..', 'graphics', 'level', 'water', 'body'),
 			'bg_tiles': import_folder_dict('..', 'graphics', 'level', 'bg', 'tiles'),
-			'cloud_small': import_folder('..', 'graphics','level', 'clouds', 'small'),
-			'cloud_large': import_image('..', 'graphics','level', 'clouds', 'large_cloud'),
+			'cloud_small': import_folder('..', 'graphics', 'level', 'clouds', 'small'),
+			'cloud_large': import_image('..', 'graphics', 'level', 'clouds', 'large_cloud'),
 		}
-		self.font = pygame.font.Font(join('..', 'graphics', 'ui', 'runescape_uf.ttf'), 40)
+		self.font = pygame.font.Font(
+		    join('..', 'graphics', 'ui', 'runescape_uf.ttf'), 40)
 		self.ui_frames = {
-			'heart': import_folder('..', 'graphics', 'ui', 'heart'), 
-			'coin':import_image('..', 'graphics', 'ui', 'coin')
+			'heart': import_folder('..', 'graphics', 'ui', 'heart'),
+			'coin': import_image('..', 'graphics', 'ui', 'coin')
 		}
 		# self.overworld_frames = {
 		# 	'palms': import_folder('..', 'graphics', 'overworld', 'palm'),
@@ -88,15 +99,26 @@ class Game:
 		self.audio_files = {
 			'coin': pygame.mixer.Sound(join('..', 'audio', 'coin.wav')),
 			'attack': pygame.mixer.Sound(join('..', 'audio', 'attack.wav')),
-			'jump': pygame.mixer.Sound(join('..', 'audio', 'jump.wav')), 
+			'jump': pygame.mixer.Sound(join('..', 'audio', 'jump.wav')),
 			'damage': pygame.mixer.Sound(join('..', 'audio', 'damage.wav')),
 			'pearl': pygame.mixer.Sound(join('..', 'audio', 'pearl.wav')),
 		}
 		self.bg_music = pygame.mixer.Sound(join('..', 'audio', 'starlight_city.mp3'))
 		self.bg_music.set_volume(0.2)
 
+	def draw_text(self, text, font, text_color, x, y):
+		# Draw a temporary background for debugging
+		bg_rect = pygame.Rect(x - 5, y - 5, font.render(text, True, (0, 0, 0)).get_width() + 10, font.render(text, True, (0, 0, 0)).get_height() + 10)
+		pygame.draw.rect(self.display_surface, (255, 255, 255), bg_rect)  # Draw white background
+
+		img = font.render(text, True, text_color)
+		self.display_surface.blit(img, (x, y))
+
+
 	def check_game_over(self):
 		if self.data.health <= 0:
+			self.draw_text("GAME OVER", font_large, (255,255,255), 130, 200)
+			print("Game Over")
 			pygame.quit()
 			sys.exit()
 
